bc6f5dec2c408960535c79a38ae1db85cfc88d1c
Rohit <rohitjobeis@gmail.com>
2022-02-01 08:56:51 +0530

    Screenshot added

--- js/custom.js
+++ js/custom.js
@@ -18,78 +18,53 @@ sr.reveal(".row2", {
   duration: 2000,
   origin: "right",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".port_img1", {
   duration: 2000,
   origin: "top",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".port_img2", {
   duration: 2000,
   origin: "bottom",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".port_left", {
   duration: 2000,
   origin: "left",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".port_right", {
   duration: 2000,
   origin: "right",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".blog1", {
   duration: 2000,
   origin: "bottom",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".cont_col1", {
   duration: 2000,
   origin: "right",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".cont_col2", {
   duration: 2000,
   origin: "left",
   distance: "300px",
-  overFlow: "hidden",
   viewFacor: 0.2,
 });
 sr.reveal(".contact-form", {
   duration: 2000,
   origin: "top",
   distance: "300px",
-  overFlow: "hidden",
-  viewFacor: 0.2,
-});
-sr.reveal(".form_col1", {
-  duration: 2000,
-  origin: "left",
-  distance: "300px",
-  overFlow: "hidden",
-  delay: 1000,
-  viewFacor: 0.2,
-});
-sr.reveal(".form_col2", {
-  duration: 2000,
-  origin: "right",
-  distance: "300px",
-  overFlow: "hidden",
-  delay: 1000,
   viewFacor: 0.2,
 });
